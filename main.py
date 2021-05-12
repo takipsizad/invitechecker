1397de31d977183db8cb1777cfb2f141c8f65040
takipsizad <addeniz25@gmail.com>
2021-05-12 19:12:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,36 @@ from flask import Flask
 from threading import Thread
 import pymongo
 from pymongo import MongoClient
+from dotenv import load_dotenv
+from flask import Response
+import os
+import json
 
-app=Flask("")
-MongoClient("h")
+load_dotenv()
+
+app = Flask("")
+client = MongoClient(os.getenv("db"))
+mydb = client["db"]
+blacklist = mydb.blacklist
+
 
 @app.route("/")
 def index():
     return "render.template"
 
+
 @app.route("/checkid")
 def check():
-    _id = request.args.get('id')
-    if _id is not None:
-        return "{}".format(_id)
+    _id_ = request.args.get('id')
+    if _id_ is not None:
+        data = {'server': int(_id_)}
+        dt = blacklist.find_one(data)
+        return flask.jsonify(
+            server=dt["server"],
+            reason=dt["reason"],
+        )
     else:
-        return flask.jsonify({"invalid":"invalid"})
+        return flask.jsonify({"invalid": "invalid"})
 
 
-Thread(target=app.run,args=("0.0.0.0",80)).start()
+Thread(target=app.run, args=("0.0.0.0", 80)).start()
